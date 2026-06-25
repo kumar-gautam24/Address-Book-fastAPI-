c7eef0e010edd2634da310f0fef9a475c256753a
Gautam Kumar <gk24feb@gmail.com>
2026-06-25 16:20:50 +0530

    feat: add update, delete and get_all to repository layer

--- app/repository.py
+++ app/repository.py
@@ -2,7 +2,6 @@
 
 """
 import logging
-from typing import Optional
 
 from .database import get_connection
 
@@ -20,11 +19,26 @@ def create(data: dict) -> dict:
         "INSERT INTO address_book (city,latitude,longitude) VALUES (?,?,?) RETURNING *",
         (data["city"],data["latitude"],data["longitude"]),
         )
-        conn.commit()
+
         row= cur.fetchone()
+        conn.commit()
         return dict(row)
     finally:
         conn.close()
+
+
+def get_all()-> list[dict] :
+    conn = get_connection()
+    try:
+        cur = conn.execute(
+        "SELECT * FROM address_book",
+        )
+        rows = cur.fetchall()
+        return  [dict(row) for row in rows]
+    finally:
+        conn.close()
+
+
 
 
 def get_by_id(address_id: int) -> dict | None:
@@ -35,8 +49,37 @@ def get_by_id(address_id: int) -> dict | None:
         "SELECT * FROM address_book WHERE id = ?",
         (address_id,),
         )
-        conn.commit()
+
         row= cur.fetchone()
+
+        logger.info(row)
         return dict(row) if row else None
+    finally:
+        conn.close()
+
+def update_address(*,address_id:int,data:dict)->dict|None:
+    conn = get_connection()
+    try:
+        cur= conn.execute(
+            "UPDATE address_book SET city=?,latitude=?,longitude=?,updated_at=CURRENT_TIMESTAMP WHERE id =? RETURNING *",
+            (data["city"],data["latitude"],data["longitude"],address_id)
+        )
+        row= cur.fetchone()
+        conn.commit()
+        return dict(row) if row else None
+
+    finally:
+        conn.close()
+
+
+def delete_address(address_id:int)->bool:
+    conn =  get_connection()
+    try:
+        cur = conn.execute(
+            "DELETE FROM address_book WHERE id = ?",
+            (address_id,)
+        )
+        conn.commit()
+        return  cur.rowcount== 1
     finally:
         conn.close()
